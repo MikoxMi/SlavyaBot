11b16f70b0e0d270b74123f9d88c7982dc749e31
MikoxMi <mrx22661@gmail.com>
2019-06-28 10:32:29 +0400

    Problem has been resolved. Wrong variable on Heroku

--- cogs/utils/u_mongo.py
+++ cogs/utils/u_mongo.py
@@ -6,7 +6,6 @@ class Mongo():
     def __init__(self):
         self.url = str(os.getenv('URL_MONGO'))
         self.client = MongoClient(host=self.url, connectTimeoutMS=30000, connect=False, serverSelectionTimeoutMS=30000)
-        print(self.client)
         self.db = self.client.get_database(name="weekbd")
         self.collections = {'members': self.db.members,
                             'server_settings': self.db.server_settings,
